2be15df945f3ac2fccdb3067718a5fab4ff2f172
gonzalo <gonzalolopecc@gmail.com>
2022-09-28 13:28:16 +0200

    more advances

--- logical_induction/database_connection.py
+++ logical_induction/database_connection.py
@@ -17,6 +17,11 @@ class KGHandler():
     def _create_relation(tx: ManagedTransaction ,from_name: str, to_name: str,relation_type: str, relation_attributes: dict):
         query = f'MATCH (n1 {{name: "{from_name}"}}),(n2 {{name: "{to_name}"}}) CREATE (n1)-[r:{relation_type} $attrs]->(n2) '
         tx.run(query,attrs = relation_attributes)
+    
+    @staticmethod
+    def _set_label(tx: ManagedTransaction, object_name: str, labels: list):
+        query = f'MATCH (n1 {{name: "{object_name}"}}) SET n1{":"*(len(labels)>0) + ":".join(labels)}'
+        tx.run(query)
 
     def create_object(self, object_attributes:dict , labels:list = []):
         with self.driver.session(database='objects') as session:
@@ -26,10 +31,20 @@ class KGHandler():
         with self.driver.session(database='objects') as session:
             session.write_transaction(self._create_relation,from_name, to_name, relation_type, relation_attributes)
     
+    def create_label_relation(self,from_label_name: str, to_label_name: str,relation_type: str, relation_attributes: dict = {}):
+        with self.driver.session(database='labels') as session:
+            session.write_transaction(self._create_relation,from_label_name, to_label_name, relation_type, relation_attributes)
 
+    def create_label(self, object_attributes:dict , labels:list = []):
+        with self.driver.session(database='labels') as session:
+            session.write_transaction(self._create_object,object_attributes,labels)
+    
+    def add_labels_to_object(self,object_name: str,labels: list = []):
+        with self.driver.session(database='objects') as session:
+            session.write_transaction(self._set_label,object_name,labels)
 
 hand = KGHandler('bolt://localhost:7687', 'neo4j', '')
 
-hand.create_relation('test name', 'test_name','TEST_RELATION', {'test_attr':'tre'})
+hand.add_labels_to_object()
 
-hand.close()
+# hand.close()
