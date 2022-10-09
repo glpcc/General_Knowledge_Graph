ea7eca88f23f4216b884c08c006bfafecdcee122
gonzalo <gonzalolopecc@gmail.com>
2022-10-09 21:46:21 +0200

    advanced

--- src/database_connection.py
+++ src/database_connection.py
@@ -55,22 +55,37 @@ class KGHandler():
         for attributes,labels in objects:
             self.create_object(attributes,labels)
 
-    def instanciate_full_relation(self,relation_name: str, related_relation_objects: dict):
+    def instanciate_full_relation(self,relation_name: str, related_relation_objects_names: dict):
         with self.driver.session(database='knowledgegraph') as session:
+            # Check if the relation class exists
             relation_exists = session.run('OPTIONAL MATCH (n:RelationConcept { name: $name }) RETURN n IS NOT NULL AS Predicate', name=relation_name).value()
             if relation_exists:
                 relationConcept_relations = session.run('MATCH (n:RelationConcept { name: $name })-[r]->(c) RETURN r',name=relation_name)
                 relationConcept_relations = [i['r'].type for i in relationConcept_relations]
-                if all(i in related_relation_objects for i in relationConcept_relations):
-                    session.run('MATCH (r2:RelationConcept { name: $name }) CREATE (r:Relation { name: $name}),(r)-[:INSTANCE_OF]->(r2)',name=relation_name)
-                    for r in relationConcept_relations:
-                        session.run(f'MATCH (n:Object {{ name: $objectname}}),(r:Relation {{ name: $relationConceptName}}) CREATE (n)-[:{r}]->(r)',
-                            objectname = related_relation_objects[r],
-                            relationConceptName = relation_name,
-                        )
+                # Check if all relations of the RelationConcept are in the related_relation_objects_names dict
+                # TODO try to implement optional relations
+                if all(i in related_relation_objects_names for i in relationConcept_relations):
+                    # Check if the objects involved exists
+                    objects_exits = session.run(f'MATCH (n:Object) WHERE n.name IN $names RETURN count(n) = $nameCount',
+                        names = [related_relation_objects_names[i] for i in relationConcept_relations],
+                        nameCount = len(relationConcept_relations)
+                    ).value()
+                    if objects_exits:
+                        session.run('MATCH (r2:RelationConcept { name: $name }) CREATE (r:Relation { name: $name}),(r)-[:INSTANCE_OF]->(r2)',name=relation_name)
+                        for r in relationConcept_relations:
+                            session.run(f'MATCH (n:Object {{ name: $objectname}}),(r:Relation {{ name: $relationConceptName}}) CREATE (n)-[:{r}]->(r)',
+                                objectname = related_relation_objects_names[r],
+                                relationConceptName = relation_name,
+                            )
+                    else:
+                        print(f'Not all the objects given exists in the graph')
+                else:
+                    print(f'Not all relations of the RelationConcept where given, ther relations needed are {relationConcept_relations}')
             else:
                 print(f'The Relation {relation_name} doesnt exist as a relation concept, try creating the relaton concept first')
 
+    def create_relation_concept():
+        ...
 
     def create_relation(self,from_name: str, to_name: str,relation_type: str, relation_attributes: dict):
         with self.driver.session(database='knowledgegraph') as session:
